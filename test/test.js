c97a16f8a8374bd5094b997c2c378de6bb04d803
alan <alanmorataya19@gmail.com>
2021-06-04 20:02:12 -0600

    cambio4

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 
 describe('Test inicial', () => {
-  it('Deberia verificar si la respuesta es verdadera', () => {
+  it('Deberia verificar si la respuesta va a ser verdadera', () => {
     expect(true).to.be.true;     
   });
 }); 
